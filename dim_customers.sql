525398364380873fafc86eaaf3f7d7141c092740
Kaio Farias <kaio.santos.farias@gmail.com>
2025-12-06 18:52:04 -0300

    Delete dim_customers.sql

--- dim_customers.sql
+++
@@ -1,8 +0,0 @@
--- Please edit the sample below
-
-CREATE MATERIALIZED VIEW dim_customers AS
-SELECT DISTINCT
-  `Customer ID` as customer_id,
-  `Customer Name` as customer_name,
-  `Segment` as segment
-FROM workspace.default.superstore_masterdata
